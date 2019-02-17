9374ad90470c98733d9430cbfa16c3a16b0ed677
Prakhar Kulshrestha <prakhar.kulshrestha1801@gmail.com>
2019-02-17 10:38:13 -0500

    Add files via upload

---
+++ TreeDS.py
@@ -0,0 +1,19 @@
+class Node:
+	def __init__(self,dataval=None):
+		self.dataval = dataval
+		self.leftval = None
+		self.rightval
+
+class TreeDS:
+	def __init__(self):
+		self.headval = None
+
+	def build123Tree(self):
+		
+
+
+
+
+
+tree=TreeDS()
+
