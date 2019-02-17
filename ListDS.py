6e0f4fad8770428ce12c1c69d014aab9c07ec2a4
Prakhar Kulshrestha <prakhar.kulshrestha1801@gmail.com>
2019-02-16 19:14:49 -0500

    ListDs
    Added more list functions like adding a new node at the end/at a specific position

--- ListDS.py
+++ ListDS.py
@@ -27,8 +27,29 @@ class ListDS:
 		self.headval=listds
 		self.headval.nextval=temp
 		
-		 
-
+		
+	def addToTheEnd(self,listds):
+		current=self.headval
+		while current.nextval is not None:
+			current=current.nextval
+		current.nextval=listds
+	
+	def insertAtSpecificPosition(self,listds,pos):
+		current=self.headval
+		count=0
+		while current.nextval is not None:
+			current=current.nextval
+			count+=1
+			print("Count is ",count)		
+			if count == pos:
+				break
+		temp=current.nextval
+		prev=current
+		current=listds
+		prev.nextval=current
+		current.nextval=temp
+		
+	
 list1 = ListDS()
 list1.headval = Node("Mon")
 e2 = Node("Tues")
@@ -41,3 +62,12 @@ e4=Node("Sun")
 list1.addToBeginning(e4)
 list1.printList()
 print("List length after adding new node at the beginning: ",list1.length())
+e5=Node("Thur")
+list1.addToTheEnd(e5)
+list1.printList()
+print("List length after adding new node at the end: ",list1.length())
+e6=Node("Fri")
+pos=3
+list1.insertAtSpecificPosition(e6,pos)
+list1.printList()
+print("List length after adding new node at the position: ",pos,list1.length())
